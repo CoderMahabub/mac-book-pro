c1f4fa34db456f26737f35dcbbe0fc5e761fa7ad
CoderMahabub <codermahabub@gmail.com>
2021-08-21 14:08:22 +0600

    Script File Edit and Update

--- js/app.js
+++ js/app.js
@@ -1,3 +1,11 @@
+//Function To Get Total Price
+function getTotalPrice(product, bestPriceTotal, memoryCostTotal, storageCostTotal, deliveryCostTotal) {
+    const totalPrice = document.getElementById(product);
+    let totalPriceValue = totalPrice.innerText;
+    totalPriceValue = bestPriceTotal + memoryCostTotal + storageCostTotal + deliveryCostTotal;
+    totalPrice.innerText = totalPriceValue;
+};
+
 //Function to Minimize Calculationo for each button click to one function
 function calculateTotal() {
     const bestPrice = document.getElementById('best-price');
@@ -12,43 +20,38 @@ function calculateTotal() {
     const deliveryCost = document.getElementById('delivery-cost');
     const deliveryCostTotal = parseFloat(deliveryCost.innerText);
 
-    const totalPrice = document.getElementById('total-price');
-    let totalPriceValue = totalPrice.innerText;
-    totalPriceValue = bestPriceTotal + memoryCostTotal + storageCostTotal + deliveryCostTotal;
-    totalPrice.innerText = totalPriceValue;
-
-    const finalTotal = document.getElementById('final-total');
-    let finatToalValue = finalTotal.innerText;
-    finatToalValue = bestPriceTotal + memoryCostTotal + storageCostTotal + deliveryCostTotal;
-    finalTotal.innerText = finatToalValue;
-}
+    //get total Price for both fields
+    getTotalPrice('total-price', bestPriceTotal, memoryCostTotal, storageCostTotal, deliveryCostTotal);
+    getTotalPrice('final-total', bestPriceTotal, memoryCostTotal, storageCostTotal, deliveryCostTotal);
+};
 
 //Function To Minimize Zero Cost Items
-function zeroCost(product) {
-    const memoryCost = document.getElementById(product + '-cost');
-    memoryCost.innerText = 0;
+function getZeroCost(product) {
+    const productCost = document.getElementById(product + '-cost');
+    productCost.innerText = 0;
     calculateTotal();
-}
+};
+
 //Function To Minimize 180 Cost Items
-function oneEightyCost(product) {
-    const memoryCost = document.getElementById(product + '-cost');
-    memoryCost.innerText = 180;
+function getOneEightyCost(product) {
+    const productCost = document.getElementById(product + '-cost');
+    productCost.innerText = 180;
     calculateTotal();
-}
+};
 
 //click Handler for memory-8gb
 document.getElementById('memory-8gb').addEventListener('click', function () {
-    zeroCost('memory');
+    getZeroCost('memory');
 });
 
 //click Handler for memory-16gb
 document.getElementById('memory-16gb').addEventListener('click', function () {
-    oneEightyCost('memory');
+    getOneEightyCost('memory');
 });
 
 //Click Handler for storage-256gb
 document.getElementById('storage-256gb').addEventListener('click', function () {
-    zeroCost('storage');
+    getZeroCost('storage');
 });
 
 //Click Handler for storage-256gb
@@ -60,12 +63,12 @@ document.getElementById('storage-512gb').addEventListener('click', function () {
 
 //Click Handler for storage-1tb
 document.getElementById('storage-1tb').addEventListener('click', function () {
-    oneEightyCost('storage');
+    getOneEightyCost('storage');
 });
 
 //Click Handler for Delivery Charge
 document.getElementById('free-delivery').addEventListener('click', function () {
-    zeroCost('delivery');
+    getZeroCost('delivery');
 });
 
 //Click Handler for Delivery Charge
@@ -79,16 +82,24 @@ document.getElementById('premium-delivery').addEventListener('click', function (
 document.getElementById('promo-btn').addEventListener('click', function () {
     const promoInput = document.getElementById('promo-input');
     const promoInputValue = promoInput.value;
-    let finalTotal = document.getElementById('final-total');
-    let finalTotalPrice = parseFloat(finalTotal.innerText);
 
+    const totalPrice = document.getElementById('total-price');
+    const totalPriceValue = parseFloat(totalPrice.innerText);
+
+    const finalPrice = document.getElementById('final-total');
+    const finalTotalValue = parseFloat(finalPrice.innerText);
+    console.log(finalTotalValue);
+
+    //Promo Code = "stevekaku" for 20% discount
     if (promoInputValue.toLowerCase() == 'stevekaku') {
-        const totalAfterDiscount = (finalTotalPrice / 100) * 80;
-        finalTotal.innerText = totalAfterDiscount;
+        const totalAfterDiscount = (totalPriceValue / 100) * 80;
+        finalPrice.innerText = totalAfterDiscount;
+
+        //To show round figure after 20% discount
+        finalPrice.innerText = Math.round(totalAfterDiscount);
         promoInput.value = '';
     } else {
-        alert("Code Doesn't Match");
+        alert("Promo Code Doesn't Match");
         promoInput.value = '';
     }
-
 });
